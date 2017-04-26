645997a04519ef05995d26216b0a2dc04e5cabd3
Emma Kopf <emmakopf@gmail.com>
2017-04-26 02:21:47 -0400

    fixed NeXtflix references

--- what_to_watch.php
+++ what_to_watch.php
@@ -22,7 +22,7 @@
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
 </button>
-<a class="navbar-brand" href="profile.html" style="color:white">NeXtflix</a>
+<a class="navbar-brand" href="profile.php" style="color:white">NeXtflix</a>
 </div>
 
 <div class="collapse navbar-collapse" id="topNavBar">
